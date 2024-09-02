03b4deadb7177c07936f719e07de515b362d8e85
unknown <meganathan212003@gmail.com>
2024-09-03 00:08:38 +0530

    Create a new method

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -9,7 +9,8 @@
 import { Request, Response } from "express";
 import Task from "../models/task.model";
 import TaskService from "../services/task.service";
-import { error } from "console";
+import { STRING } from "sequelize";
+
 
 
 export const getTasks = async (req: Request, res: Response) => {
@@ -27,4 +28,35 @@ export const createTask = async (req: Request, res: Response) => {
     const task = Task.create(req.body);
     res.json(task);
 
+}
+
+// Get task by id
+export const getTaskById = async (req: Request, res: Response) => {
+
+    try {
+
+        // Get the id from the url params
+        const id = req.params.id;
+        const task = TaskService.getTaskById(id);
+        return res.json(task);
+
+    }
+    catch (e) {
+        console.error(e);
+
+    }
+}
+
+// Also create for delete task
+export const deleteTask = async (req: Request, res: Response) => {
+    try {
+
+        const id = req.params.id;
+        const deleteTask = TaskService.deleteTaskById(id);
+        return res.json(deleteTask);
+    }
+    catch (e) {
+        console.error(e);
+
+    }
 }
--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -15,6 +15,32 @@ export class TaskService {
     async createTask(taskData: { title: string, description: string, status?: string }) {
         return await Task.create(taskData);
     }
+
+    // Get task by id
+    async getTaskById(id: string) {
+        const numberId = Number(id);
+        return await Task.findByPk(numberId);
+    }
+
+    // Write the delete task
+    async deleteTaskById(id: string) {
+        const numberId = Number(id);
+        // Check if the conversion was successful and id is a valid number
+        if (isNaN(numberId)) {
+            throw new Error('Invalid ID format');
+        }
+        const result = await Task.destroy({
+            where: {
+                id: numberId
+            }
+        });
+        // Check if any rows were deleted
+        if (result === 0) {
+            throw new Error('Task not found');
+        }
+
+        return { message: 'Task deleted successfully' };
+    }
 }
 
 // Export the new obje in TaskService
